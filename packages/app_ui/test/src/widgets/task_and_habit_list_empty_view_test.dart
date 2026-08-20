e7f46cc407404037f85120c88925469bcb9f7d48
KiwiComp <kim.bellander@gmail.com>
2026-08-20 17:55:38 +0200

    Fixed app_ui test coverage

---
+++ packages/app_ui/test/src/widgets/task_and_habit_list_empty_view_test.dart
@@ -0,0 +1,20 @@
+import 'package:app_ui/app_ui.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../helpers/pump_app.dart';
+
+void main() {
+  group('TaskAndHabitListEmptyView', () {
+    testWidgets('renders the title and text', (tester) async {
+      await tester.pumpApp(
+        TaskAndHabitListEmptyView(
+          title: 'No items yet',
+          text: 'Tap the plus button to add one',
+        ),
+      );
+
+      expect(find.text('No items yet'), findsOneWidget);
+      expect(find.text('Tap the plus button to add one'), findsOneWidget);
+    });
+  });
+}
